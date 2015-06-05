93e010b1bcffd50006fb35a2a241b67f1f0fe470
Thibaud DELOBELLE TOUSSAINT <thibaud.delobelle@etu.univ-lille1.fr>
2015-06-05 11:05:40 +0200

    Modifcation API lesson

---
+++ src/main/java/fr/lamphi/api/Lesson.java
@@ -0,0 +1,66 @@
+package fr.lamphi.api;
+
+public class Lesson {
+
+	private String title;
+	private String author;
+	private String creationDate;
+	private boolean certified;
+	private int id;
+	
+	public Lesson(){
+		
+	}
+	
+	public Lesson(String titre, int id, String auteur, String dateCreate, boolean isValide){
+		this.title=titre;
+		this.id=id;
+		this.author=auteur;
+		this.creationDate=dateCreate;
+		this.certified=isValide;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public String getCreationDate(){
+		return creationDate;
+	}
+	
+	public boolean getCertified(){
+		return certified;
+	}
+	
+	public void setTitle(String title){
+		this.title=title;
+	}
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	
+	public void setAuthor(String author){
+		this.author=author;
+	}
+	
+	public void setCreationDate(String creationDate){
+		this.creationDate=creationDate;
+	}
+	
+	public void setValide(boolean certified){
+		this.certified=certified;
+	}
+	
+	public void setCertified(boolean certified) {
+		this.certified=certified;
+	}
+}
---
+++ src/test/java/fr/lamphi/api/LessonTest.java
@@ -0,0 +1,7 @@
+package fr.lamphi.api;
+
+import org.glassfish.jersey.test.JerseyTest;
+
+public class LessonTest extends JerseyTest {
+
+}
