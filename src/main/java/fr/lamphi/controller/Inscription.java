4c969bf6f120c9b397cfb3e13c38159157931273
Aicha DIAGNE <aichaodette@gmail.com>
2015-06-05 16:50:35 +0200

    inscription

---
+++ src/main/java/fr/lamphi/controller/Inscription.java
@@ -0,0 +1,26 @@
+package fr.lamphi.controller;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet(name = "inscriptionServlet", urlPatterns = { "/inscription" }, initParams = { @WebInitParam(name = "simpleParam", value = "paramValue") })
+public class Inscription extends HttpServlet {
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
