b1cb12b4cf1b164f8ba96ffeee8ef7b4806eb510
Thibaud DELOBELLE TOUSSAINT <thibaud.delobelle@etu.univ-lille1.fr>
2015-06-10 10:37:46 +0200

    modif

---
+++ src/main/java/fr/lamphi/controller/EditLesson.java
@@ -0,0 +1,39 @@
+package fr.lamphi.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import fr.lamphi.api.lesson.Lesson;
+import fr.lamphi.api.lesson.LessonDBRessource;
+import fr.lamphi.api.lesson.LessonRessource;
+import fr.lamphi.api.user.User;
+
+@WebServlet(name = "editLessonServlet", urlPatterns = { "/EditLesson" }, initParams = { @WebInitParam(name = "simpleParam", value = "paramValue") })
+public class EditLesson extends HttpServlet {
+
+	
+	protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+
+        User user = (User)request.getSession().getAttribute("user");
+        
+        if(user == null)
+			response.sendRedirect("register.jsp");
+        
+        Lesson lesson = new Lesson(0,request.getParameter("title"), request.getParameter("contenu"), user.getId(), ""+System.currentTimeMillis(),1, false,0,0);
+
+
+        System.out.println(lesson);
+        LessonDBRessource lr = new LessonDBRessource();
+        lr.createLesson(lesson);
+       
+        response.sendRedirect("index.jsp");
+	}
+}
